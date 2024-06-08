dcacf5ee58ba0e72d86e6289314abcc259921b34
Abhijeet Kokat <118294497+Abhijeetkokat007@users.noreply.github.com>
2024-06-08 20:52:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
    <> 
-     <div className="App">
+     <div className="App font-bold">
      <h1>Home Page</h1>
      <h1>About Page</h1> 
     <h1>Service Page</h1> 
